c29f6dd6f66f2fbfe2ae4a8f78e69478aba82325
Ramachandran Varadaraju <rajram92@hotmail.com>
2024-10-08 17:45:16 +0530

    Initial Commit

---
+++ FirebaseAuthPrac/FirebaseAuthPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthPracApp.swift
+//  FirebaseAuthPrac
+//
+//  Created by Ramachandran Varadaraju on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
